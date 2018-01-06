d7bf13196d9b672443396d3983f538efb73f0e2c
Karol Piątek <kpiatek40@gmail.com>
2018-01-06 14:44:09 +0100

    Xib to StoryBoard update

--- FavoritePlaces/FavoritePlaces/Supporting Files/AppDelegate.swift
+++ FavoritePlaces/FavoritePlaces/Supporting Files/AppDelegate.swift
@@ -14,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow()
-        let navigationController = UINavigationController()
-        window?.rootViewController = navigationController
-        navigationController.setViewControllers([AddFavoritePlaceVC()], animated: true)
-        window?.makeKeyAndVisible()
         return true
     }
 }
